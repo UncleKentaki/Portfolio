28ccd6f78ff897e3b6de9bc46a278340bf2440fe
Sean Naylor <seannnaylor@gmail.com>
2021-05-30 14:08:50 -0700

    initial commit

---
+++ assets/js/helloWorld.js
@@ -0,0 +1,9 @@
+// testang
+function puddingHole() {
+    let pudding = true;
+    pudding = false;
+    let asshole = 42;
+    let puddinghole = pudding + " " + asshole;
+    console.log(puddinghole);
+}
+puddingHole();
